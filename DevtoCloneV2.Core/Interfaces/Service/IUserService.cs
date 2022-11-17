6d3de5a00c0570e3d52325d7fa99130d40767fc5
Matthew Palacio <mpalacio@catalyte.io>
2022-11-17 15:26:08 -0800

    creates user service interface

---
+++ DevtoCloneV2.Core/Interfaces/Service/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using DevtoCloneV2.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevtoCloneV2.Core.Interfaces.Service
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<User>> GetAllUsers();
+        public Task<User> GetUserByEmail(int email);
+        public Task<User> GetUserByEmail(string email);
+        public Task CreateUser(User user);
+        public Task UpdateUser(int id, User user);
+        public Task DeleteUser(int id);
+    }
+}
