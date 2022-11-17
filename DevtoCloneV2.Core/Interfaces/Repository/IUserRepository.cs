11240bf8c894834cb8ca51841becbc5f3b494680
Matthew Palacio <mpalacio@catalyte.io>
2022-11-17 13:33:23 -0800

    creates user repository

---
+++ DevtoCloneV2.Core/Interfaces/Repository/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevtoCloneV2.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevtoCloneV2.Core.Interfaces.Repository
+{
+    public interface IUserRepository
+    {
+        public Task<IEnumerable<User>> GetAllUsers();
+        public Task<User?> GetUserById(object id);
+        public void CreateUser(User user);
+        public void CreateUsers(IEnumerable<User> users);
+        public void UpdateUser(User user);
+        public void DeleteUser(User user);
+    }
+}
---
+++ DevtoCloneV2.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using DevtoCloneV2.Core.Entities;
+using DevtoCloneV2.Core.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevtoCloneV2.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppContext _context;
+
+        public UserRepository(AppContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetUserById(object id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public void CreateUser(User user)
+        {
+            _context.Users.Add(user);
+        }
+
+        public void CreateUsers(IEnumerable<User> users)
+        {
+            _context.Users.AddRange(users);
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Users.Attach(user);
+            _context.Entry(user).State = EntityState.Modified;
+        }
+
+        public void DeleteUser(User user)
+        {
+            if (_context.Entry(user).State is EntityState.Modified)
+            {
+                _context.Users.Attach(user);
+            }
+            _context.Users.Remove(user);
+        }
+    }
+}
